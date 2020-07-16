646ccb3f76ad6b73f5ea4347c01c733f2452a7e2
rirtakmanna <web@rirtakmanna.com>
2020-07-16 23:23:26 +0530

    All Done

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
-console.log('Hello World')
+if (document.getElementById("form")) {
+  const form = document.getElementById("form");
+  const username = document.getElementById("username");
+  const password = document.getElementById("password");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (username.value === "sajalBain" && password.value === "password") {
+      window.location.href = "./switch.html";
+    } else {
+      alert("Username or password error");
+    }
+  });
+}
